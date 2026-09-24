a84f7f49040f31d381599c712faabeacd1ccb0da
yousefIbrahem <ssnerd47@gmail>
2026-09-24 16:32:46 +0300

    add home screen

--- lib/features/home_screen/presentation/home_screen.dart
+++ lib/features/home_screen/presentation/home_screen.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:money_tracker_app/features/home_screen/models/home_screen_model.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
+  const HomeScreen({super.key, required this.homeScreenModel});
+  final HomeScreenModel homeScreenModel;
   @override
   Widget build(BuildContext context) {
+    final breakdownEntries = homeScreenModel.planSummary.entries.toList();
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Name of the Plan'),
-          GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(homeScreenModel.planName),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemCount: breakdownEntries.length,
+                itemBuilder: (context, index) {
+                  final entry = breakdownEntries[index];
+                  return Card(
+                    child: Column(
+                      children: [Text(entry.key), Text('${entry.value}')],
+                    ),
+                  );
+                },
+              ),
             ),
-            itemBuilder: (context, index) {
-              return Card();
-            },
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
